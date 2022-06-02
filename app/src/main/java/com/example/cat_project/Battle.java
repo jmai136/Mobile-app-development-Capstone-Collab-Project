963b15d1bdaafd6e2d0ff02803aefd4a6c94c788
maict24 <maitmc24@gmail.com>
2022-06-02 15:23:34 -0700

    Made superclass, removed some unnecessary methods.

--- app/src/main/java/com/example/cat_project/Battle.java
+++ app/src/main/java/com/example/cat_project/Battle.java
@@ -20,7 +20,7 @@ public class Battle extends AppCompatActivity {
     private RadioGroup radioGroup;
     private TextView txtTimer, txtPlayerHP, txtEnemyHP;
 
-    final private Cat cat = new Cat(rng.nextInt(100 - 20) + 20);
+    final private Cat cat = new Cat();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +49,7 @@ public class Battle extends AppCompatActivity {
         // countdown timer, 20 seconds is equal to 20000 milliseconds
         long duration = 20000;
 
-        final Mouse mouse = new Mouse(rng.nextInt(150 - 100) + 100);
+        final Mouse mouse = new Mouse(100, 150);
 
         new CountDownTimer(duration, 1000) {
              public void onTick(long millisUntilFinished) {
@@ -61,7 +61,7 @@ public class Battle extends AppCompatActivity {
                  // txtTimer.setText(sDur);
 
                  // allows for battle options which should be radio groups
-                 playerBattleOption(radioGroup);
+                 cat.getBattleOptionResults(getRadioID());
 
                  Button btnLockIn = findViewById(R.id.btnLockIn);
                  btnLockIn.setOnClickListener(new View.OnClickListener() {
@@ -86,7 +86,7 @@ public class Battle extends AppCompatActivity {
                      return;
 
                  // set text timer to show what has happened
-                 txtTimer.setText(playerAttackText(radioGroup) + "\n" + mouse.mAtkTxt);
+                 txtTimer.setText(cat.getDamageText() + "\n" + mouse.mAtkTxt);
 
                  // update healths
                  txtEnemyHP.setText(mouseDmg);
@@ -121,7 +121,7 @@ public class Battle extends AppCompatActivity {
                 // txtTimer.setText(sDur);
 
                 // allows for battle options which should be radio groups
-                playerBattleOption(radioGroup);
+                // playerBattleOption(radioGroup);
 
                 Button btnLockIn = findViewById(R.id.btnLockIn);
                 btnLockIn.setOnClickListener(new View.OnClickListener() {
@@ -172,7 +172,7 @@ public class Battle extends AppCompatActivity {
                 // txtTimer.setText(sDur);
 
                 // allows for battle options which should be radio groups
-                playerBattleOption(radioGroup);
+                // playerBattleOption(radioGroup);
 
                 Button btnLockIn = findViewById(R.id.btnLockIn);
                 btnLockIn.setOnClickListener(new View.OnClickListener() {
@@ -204,117 +204,72 @@ public class Battle extends AppCompatActivity {
         }.start();
     }
 
-    // battle option
-    private int playerBattleOption(RadioGroup radioGroup) {
-        Random rng = new Random();
-
-        int rdId = radioGroup.getCheckedRadioButtonId(), dmg;
-
-        // need to figure out what are the ids of the radio buttons
-        switch (rdId) {
-            case 0:
-                dmg = rng.nextInt(15);
-                break;
-            case 1:
-                dmg = rng.nextInt(10);
-                break;
-            case 2:
-                dmg = rng.nextInt(12);
-                break;
-            case 3:
-                dmg = rng.nextInt(20);
-                break;
-            default:
-                dmg = 0;
-                break;
-        }
-
-        return dmg;
+    private int getRadioID() {
+        return radioGroup.getCheckedRadioButtonId();
     }
 
     private boolean isDead(int HP) {
         return (HP <= 0);
     }
 
-    private String playerAttackText(RadioGroup radioGroup) {
-        int att = radioGroup.getCheckedRadioButtonId();
-        String attTxt;
-
-        switch (att) {
-            case 0:
-                attTxt = "You hiss at the mouse to let him know who is boss";
-                break;
-            case 1:
-                attTxt = "You pounce towards the mouse, ready to bite if you get the chance.";
-                break;
-            case 2:
-                attTxt = "You raise your mighty claws into the air and swipe down.";
-                break;
-            case 3:
-                attTxt = "You concentrate and hit them all in one quick swipe!";
-                break;
-            default:
-                attTxt = "You missed.";
-                break;
-        }
-
-        return attTxt;
-    }
 
     // superclass
     private static class Character {
         Random rng = new Random();
 
-        protected int HP;
-
+        protected int HP, Choice, DmgMax1, DmgMax2, DmgMax3, DmgMax4;
+        protected String AtkTxt1, AtkTxt2, AtkTxt3, AtkTxt4, Missed;
         protected Pair<Integer, String> DmgAndTxtValues;
 
-        protected void getBattleOptionResults(
-                int choice, int dmgMax1, int dmgMax2, int dmgMax3, int dmgMax4,
-                String atkTxt1, String atkTxt2, String atkTxt3, String atkTxt4) {
+        protected void getBattleOptionResults(int choice) {
+            DmgAndTxtValues = setBattleOption(choice);
+        }
 
-            DmgAndTxtValues = setBattleOption(choice, dmgMax1, dmgMax2, dmgMax3, dmgMax4,
-            atkTxt1, atkTxt2, atkTxt3, atkTxt4);
+        protected int getDamage() {
+            return DmgAndTxtValues.first;
+        }
+
+        protected String getDamageText() {
+            return DmgAndTxtValues.second;
         }
 
         protected Pair<Integer, String> setBattleOption(
-                int choice, int dmgMax1, int dmgMax2, int dmgMax3, int dmgMax4,
-                String atkTxt1, String atkTxt2, String atkTxt3, String atkTxt4) {
+                int choice) {
 
             int dmg;
             String atkTxt;
 
             switch (choice) {
                 case 0:
-                    dmg = rng.nextInt(dmgMax1);
-                    atkTxt = atkTxt1;
+                    dmg = rng.nextInt(DmgMax1);
+                    atkTxt = AtkTxt1;
                     break;
                 case 1:
-                    dmg = rng.nextInt(dmgMax2);
-                    atkTxt = atkTxt2;
+                    dmg = rng.nextInt(DmgMax2);
+                    atkTxt = AtkTxt2;
                     break;
                 case 2:
-                    dmg = rng.nextInt(dmgMax3);
-                    atkTxt = atkTxt3;
+                    dmg = rng.nextInt(DmgMax3);
+                    atkTxt = AtkTxt3;
                     break;
                 case 3:
-                    dmg = rng.nextInt(dmgMax4);
-                    atkTxt = atkTxt4;
+                    dmg = rng.nextInt(DmgMax4);
+                    atkTxt = AtkTxt4;
                     break;
                 default:
                     dmg = 0;
-                    atkTxt = "MISSED.";
+                    atkTxt = Missed;
                     break;
             }
 
             return new Pair<Integer, String>(dmg, atkTxt);
         }
 
-        protected int setApplyDmg(int dmg) {
-            return HP -= dmg;
+        protected int setApplyDmg() {
+            return HP -= DmgAndTxtValues.first;
         }
 
-        protected boolean isDead(int HP) {
+        protected boolean isDead() {
             return (HP <= 0);
         }
     }
@@ -322,9 +277,18 @@ public class Battle extends AppCompatActivity {
 
     // inner classes
     public static class Cat extends Character {
+        public Cat() {
+            this.HP = rng.nextInt(100 - 20) + 20;
+            this.DmgMax1 = 15;
+            this.DmgMax2 = 10;
+            this.DmgMax3 = 12;
+            this.DmgMax4 = 20;
 
-        public Cat(int HP) {
-            this.HP = HP;
+            this.AtkTxt1 = "You hiss at the mouse to let him know who is boss";
+            this.AtkTxt2 = "You pounce towards the mouse, ready to bite if you get the chance.";
+            this.AtkTxt3 = "You raise your mighty claws into the air and swipe down.";
+            this.AtkTxt4 = "You concentrate and hit them all in one quick swipe!";
+            this.Missed = "You missed";
         }
 
         public boolean getIsDead() {
@@ -335,26 +299,36 @@ public class Battle extends AppCompatActivity {
             deathScreen();
         }
 
-        public int getDamage() {
-            return Dmg;
-        }
         private void deathScreen() {
             // switch to bad ending screen
         }
     }
 
     public static class Mouse extends Character {
-        private int HP;
-        String mAtkTxt;
+        final Mouse mousePhaseTwo[] =
+                {
+                        new Mouse(100, 150),
+                        new Mouse(100, 180),
+                        new Mouse(100, 270),
+                        new Mouse(100, 220),
+                        new Mouse(100, 230)
+                };
 
-        Random rng = new Random();
+        public Mouse(int minHP, int maxHP) {
+            this.HP = rng.nextInt((maxHP - minHP) + minHP);
+            this.DmgMax1 = 18;
+            this.DmgMax2 = 12;
+            this.DmgMax3 = 14;
+            this.DmgMax4 = 15;
 
-        public Mouse(int HP) {
-            this.HP = HP;
-            this.mAtkTxt = "";
+            this.AtkTxt1 = "The mouse finds things in the pantry to throw at you. It's really annoying, but it won't stop you.";
+            this.AtkTxt2 = "The mouse discovered a bunch of toothpicks. He is looking around.";
+            this.AtkTxt3 = "The mouse calls to all his mouse friends. They gang up on you. Mice everywhere!!";
+            this.AtkTxt4 = "They have you surrounded. Toothpicks everywhere. Is this how it ends?";
+            this.Missed = "The mice missed.";
         }
 
-        public int mouseBattleOption(int choice) {
+        /*public int mouseBattleOption(int choice) {
             int dmg;
 
             switch (choice) {
@@ -381,7 +355,7 @@ public class Battle extends AppCompatActivity {
             }
 
             return dmg;
-        }
+        }*/
 
         public int applyDmg(int dmg) {
             return HP -= dmg;
