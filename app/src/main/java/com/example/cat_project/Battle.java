484b6061b9f788de80be3a3294c8632e124dbe95
maict24 <maitmc24@gmail.com>
2022-06-03 15:06:03 -0700

    Deleted references to rat3 and 4.

--- app/src/main/java/com/example/cat_project/Battle.java
+++ app/src/main/java/com/example/cat_project/Battle.java
@@ -29,8 +29,6 @@ public class Battle extends AppCompatActivity {
     final ImageView body = (ImageView) findViewById(R.id.body);
     final ImageView rat1 = (ImageView) findViewById(R.id.rat1);
     final ImageView rat2 = (ImageView) findViewById(R.id.rat2);
-    final ImageView rat3 = (ImageView) findViewById(R.id.rat3);
-    final ImageView rat4 = (ImageView) findViewById(R.id.rat4);
     final ImageView ratAlone = (ImageView) findViewById(R.id.ratAlone);
     final ImageView killer = (ImageView) findViewById(R.id.killer);
 
