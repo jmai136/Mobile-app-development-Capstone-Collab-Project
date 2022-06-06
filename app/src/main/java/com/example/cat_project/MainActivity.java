aedf38b9aa1db03f685c2154874e5e9cb061cbcf
maict24 <maitmc24@gmail.com>
2022-06-05 21:17:23 -0700

    No need for declaring the data type again and again.

--- app/src/main/java/com/example/cat_project/MainActivity.java
+++ app/src/main/java/com/example/cat_project/MainActivity.java
@@ -31,10 +31,13 @@ public class MainActivity extends AppCompatActivity {
         final ImageView imageView = (ImageView) findViewById(R.id.imgKitchenPixel);
         final TextView kitchen1 = (TextView) findViewById(R.id.txtKitchen1);
         final TextView kitchen2 = (TextView) findViewById(R.id.txtKitchen2);
-        Button button = (Button) findViewById(R.id.btnLivingRoom);
-        Button btnPantry = (Button) findViewById(R.id.btnPantry);
-        Button btnTrash = (Button) findViewById(R.id.btnTrash);
-        Button btnCounter = (Button) findViewById(R.id.btnCounter);
+        Button button = (Button) findViewById(R.id.btnLivingRoom),
+                btnPantry = (Button) findViewById(R.id.btnPantry),
+                btnTrash = (Button) findViewById(R.id.btnTrash),
+                btnCounter = (Button) findViewById(R.id.btnCounter);
+        // Button btnPantry = (Button) findViewById(R.id.btnPantry);
+        // Button btnTrash = (Button) findViewById(R.id.btnTrash);
+        // Button btnCounter = (Button) findViewById(R.id.btnCounter);
 
         //music
         mpMusic = new MediaPlayer();
