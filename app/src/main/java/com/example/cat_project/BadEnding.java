89932a38af24753272dc8508de11a1356002bdf2
maict24 <maitmc24@gmail.com>
2022-06-05 21:46:11 -0700

    Replace timer with handler to delay showing up text on screen

--- app/src/main/java/com/example/cat_project/BadEnding.java
+++ app/src/main/java/com/example/cat_project/BadEnding.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -21,25 +22,15 @@ public class BadEnding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bad_ending);
-        // final TextView ending = (TextView) findViewById(R.id.bEnding);
-        // Button btnCon = (Button) findViewById(R.id.btnCon);
 
-        //timer to start text and btn
-        new CountDownTimer(3500, 1000) {
-            public void onTick(long millisUntilFinished) {
-                // ending.setVisibility(View.INVISIBLE);
-                // btnCon.setVisibility(View.INVISIBLE);
-            }
+        RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout);
 
-            public void onFinish() {
-                // ending.setVisibility(View.VISIBLE);
-                // btnCon.setVisibility(View.VISIBLE);
-
-                RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 Snackbar.make(relativeLayout, "Not only were you humiliated by a bunch of mice, but the shadowy figure that killed your owner has defeated you as well. What will become of you now?", Snackbar.LENGTH_INDEFINITE).setAction("Credits", view -> startActivity(new Intent(BadEnding.this, Credits.class))).show();
             }
-        }.start();
-
-        // btnCon.setOnClickListener(v -> startActivity(new Intent(BadEnding.this, Credits.class)));
+        }, 3500);
     }
 }
--- app/src/main/java/com/example/cat_project/GoodEnding.java
+++ app/src/main/java/com/example/cat_project/GoodEnding.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -21,26 +22,14 @@ public class GoodEnding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_ending);
-        // final TextView ending = (TextView) findViewById(R.id.gEnding);
-        // Button btnCon = (Button) findViewById(R.id.btnCon);
+        RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout);
 
-        //timer to start text and btn
-        new CountDownTimer(3500, 1000) {
-            public void onTick(long millisUntilFinished) {
-                // ending.setVisibility(View.INVISIBLE);
-                // btnCon.setVisibility(View.INVISIBLE);
-            }
-
-            public void onFinish() {
-                // ending.setVisibility(View.VISIBLE);
-                // btnCon.setVisibility(View.VISIBLE);
-
-                RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 Snackbar.make(relativeLayout, "You have escaped and are free to start a new life. You cannot stop thinking about the nightmarish ordeal behind you, but at least you survived! You sniff the fresh air and wonder what adventures await you...", Snackbar.LENGTH_INDEFINITE).setAction("Credits", view -> startActivity(new Intent(GoodEnding.this, Credits.class))).show();
             }
-        }.start();
-
-        //listener button takes player to credits.java
-        // btnCon.setOnClickListener(v -> startActivity(new Intent(GoodEnding.this, Credits.class)));
+        }, 3500);
     }
 }
