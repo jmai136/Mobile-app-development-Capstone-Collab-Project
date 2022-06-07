b036f6191ae6a1056c354ce3b72d7ad686042c72
maict24 <maitmc24@gmail.com>
2022-06-07 13:15:03 -0700

    Currently handler doesn't work in BadEnding and GoodEnding, fixing that now.

--- app/src/main/java/com/example/cat_project/BadEnding.java
+++ app/src/main/java/com/example/cat_project/BadEnding.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -22,10 +23,6 @@ public class BadEnding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bad_ending);
-
-        RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout);
-
-        Handler handler = new Handler();
-        handler.postDelayed(() -> Snackbar.make(relativeLayout, "Not only were you humiliated by a bunch of mice, but the shadowy figure that killed your owner has defeated you as well. What will become of you now?", Snackbar.LENGTH_INDEFINITE).setAction("Credits", view -> startActivity(new Intent(BadEnding.this, Credits.class))).show(), 3500);
+        new Handler(Looper.myLooper()).postDelayed(() -> Snackbar.make(findViewById(R.id.RelativeLayout), "Not only were you humiliated by a bunch of mice, but the shadowy figure that killed your owner has defeated you as well. What will become of you now?", Snackbar.LENGTH_INDEFINITE).setAction("Credits", view -> startActivity(new Intent(BadEnding.this, Credits.class))).show(), 3500);
     }
 }
--- app/src/main/java/com/example/cat_project/GoodEnding.java
+++ app/src/main/java/com/example/cat_project/GoodEnding.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,9 +24,6 @@ public class GoodEnding extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_ending);
-        RelativeLayout relativeLayout = findViewById(R.id.RelativeLayout);
-
-        Handler handler = new Handler();
-        handler.postDelayed(() -> Snackbar.make(relativeLayout, "You have escaped and are free to start a new life. You cannot stop thinking about the nightmarish ordeal behind you, but at least you survived! You sniff the fresh air and wonder what adventures await you...", Snackbar.LENGTH_INDEFINITE).setAction("Credits", view -> startActivity(new Intent(GoodEnding.this, Credits.class))).show(), 3500);
+        new Handler(Looper.myLooper()).postDelayed(() -> Snackbar.make(findViewById(R.id.RelativeLayout), "You have escaped and are free to start a new life. You cannot stop thinking about the nightmarish ordeal behind you, but at least you survived! You sniff the fresh air and wonder what adventures await you...", Snackbar.LENGTH_INDEFINITE).setAction("Credits", view -> startActivity(new Intent(GoodEnding.this, Credits.class))).show(), 3500);
     }
 }
