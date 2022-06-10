f3d3dbf756071edccf8e8e6489a2293d741e379c
maict24 <maitmc24@gmail.com>
2022-06-09 18:42:25 -0700

    Stop music.

--- app/src/main/java/com/example/cat_project/Credits.java
+++ app/src/main/java/com/example/cat_project/Credits.java
@@ -3,19 +3,12 @@ package com.example.cat_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.InflateException;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-
-import java.util.zip.Inflater;
 
 public class Credits extends ListActivity {
 
